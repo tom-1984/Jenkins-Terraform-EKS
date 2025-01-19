d80c5360d2b6c80573af08e3b6524a427829bd83
Kastro Kiran V <122141717+KastroVKiran@users.noreply.github.com>
2025-01-19 23:20:19 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,8 +1,8 @@
 locals {
-  region = "ap-south-1"
+  region = "us-east-1"
   name   = "kastro-eks-cluster"
   vpc_cidr = "10.123.0.0/16"
-  azs      = ["ap-south-1a", "ap-south-1b"]
+  azs      = ["us-east-1a", "us-east-1b"]
   public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
   private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
   intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
